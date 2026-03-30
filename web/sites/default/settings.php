0c00eda2f303bb44295316a22747c6701f6d3b8c
himmat981 <himmatsinghbhatia@gmail.com>
2026-03-31 00:46:04 +0530

    sdfds

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -12,7 +12,7 @@ $databases['default']['default'] = [
 
 $settings['trusted_host_patterns'] = ['.*'];
 $config['system.logging']['error_level'] = 'verbose';
-
+$settings['ai_api_base_url'] = getenv('AI_API_BASE_URL') ?: 'http://ec2-52-66-65-95.ap-south-1.compute.amazonaws.com:8000';
 
 $settings['hash_salt'] = 'C5qBeLVqfDZFK8sN9sf6kfEEpX6GmuCWZXbb0qd9DmV7fE7Hgjhae9TA_sxCXGFYgGS6jLoscA';
 $settings['config_sync_directory'] = 'sites/default/files/config_gjpXSKXO8-etluOfnaJwDV8cD7Wtn_H-MV9hVv6FC5Il2XXcoPTWGbfzSxShOiwKLpHG4FXVxA/sync';
